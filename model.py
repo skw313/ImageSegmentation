8bd201a6c2020194047637b16b23ed375c2b9e06
skw313 <56388641+skw313@users.noreply.github.com>
2021-01-22 14:02:38 +0100

    Initial commit

---
+++ model.py
@@ -0,0 +1,7 @@
+from keras.models import Models
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Activation, UpSampling2D, BatchNormalization
+from keras.optimizers import RMSprop
+
+# TODO import losses from losses file
+
+
